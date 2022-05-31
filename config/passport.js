f8780d9615eca9a7fb5d8bb33fdf6982d4963a10
Pin Hsu Chen <paul791111@gmail.com>
2022-05-31 15:13:10 +0800

    feat: add passport local

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,34 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
+const { User } = require('../models')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  try {
+    const user = await User.findOne({ email })
+    if (!user) return done(null, false, req.flash('warning_msg', '帳號或密碼輸入錯誤'))
+    const isMatched = await bcrypt.compare(password, user.password)
+    if (!isMatched) return done(null, false, req.flash('warning_msg', '帳號或密碼輸入錯誤'))
+    return done(null, user)
+  } catch (error) {
+    done(error, false)
+  }
+}))
+
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    let user = await User.findByPk(id)
+    user = await user.toJSON()
+    return done(null, user)
+  } catch (error) {
+    done(error, false)
+  }
+})
+
+module.exports = passport
