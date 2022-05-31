b18c11f8ef4d0dac3216a2780ade609a83d11973
Pin Hsu Chen <paul791111@gmail.com>
2022-05-31 17:18:22 +0800

    feat add product seed

---
+++ seeders/20220531084927-products-seed-file.js
@@ -0,0 +1,28 @@
+'use strict'
+const faker = require('@faker-js/faker').faker
+const sizeList = ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL']
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.bulkInsert('Products',
+        Array.from({ length: 50 }, () => ({
+          name: faker.commerce.product(),
+          image_link: `https://loremflickr.com/320/240/clothing/?random=${Math.random() * 100}`,
+          size: sizeList[Math.floor(Math.random() * 7)],
+          invetory: faker.random.numeric(2),
+          color: faker.color.human(),
+          description: faker.lorem.sentence(5),
+          price: faker.commerce.price(100, 200, 0),
+          category_id: Math.floor(Math.random() * 7) + 1,
+          created_at: new Date(),
+          updated_at: new Date()
+        }))
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Products', null, {})
+  }
+}
