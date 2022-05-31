a68797de33c650aa10f9312c76545ab220a2d18c
Pin Hsu Chen <paul791111@gmail.com>
2022-05-31 15:53:20 +0800

    feat: add signin & signup & logout

---
+++ controllers/user-controllers.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcryptjs')
+const { Op } = require('sequelize')
+const { User } = require('../models')
+
+const userControllers = {
+  getSigninPage: (req, res, next) => {
+    try {
+      return res.render('signin')
+    } catch (error) {
+      next(error)
+    }
+  },
+  getSignupPage: (req, res, next) => {
+    try {
+      return res.render('signup')
+    } catch (error) {
+      next(error)
+    }
+  },
+  signUp: async (req, res, next) => {
+    try {
+      const { userName, email, password, passwordCheck } = req.body
+
+      if (!email || !password || !passwordCheck) {
+        req.flash('warning_msg', '所有項目都要填寫')
+        return res.redirect('back')
+      }
+
+      if (password !== passwordCheck) {
+        req.flash('warning_msg', '確認密碼不相符')
+        return res.redirect('back')
+      }
+
+      const user = await User.findOne({
+        where: {
+          [Op.or]: [
+            { email },
+            { userName }
+          ]
+        }
+      })
+
+      if (user) {
+        req.flash('warning_msg', 'Email或Username已經註冊過了')
+        return res.redirect('back')
+      }
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      User.create({ userName, email, password: hash })
+      req.flash('success_messages', '成功註冊帳號！')
+      res.redirect('/signin')
+    } catch (error) {
+      next(error)
+    }
+  },
+  logout: async (req, res, next) => {
+    try {
+      req.flash('success_messages', '登出成功！')
+      req.logout()
+      res.redirect('/signin')
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = userControllers
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const { generalErrorHandler } = require('../middleware/error-handler')
+const userControllers = require('../controllers/user-controllers')
 
-router.get('/home', (req, res) => res.render('home'))
-router.use('/', (req, res) => res.redirect('/home'))
+router.get('/signup', userControllers.getSignupPage)
+router.post('/signup', userControllers.signUp)
+router.get('/signin', userControllers.getSigninPage)
+router.get('/logout', userControllers.logout)
+
+router.use('/', generalErrorHandler)
+router.use('/', (req, res) => res.redirect('/products'))
 
 module.exports = router
