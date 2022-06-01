6b557e4e5aa3cfaf9959a7fab0ffff421ecb769b
Pin Hsu Chen <paul791111@gmail.com>
2022-06-01 12:41:21 +0800

    feat: add user products page

--- controllers/product-controllers.js
+++ controllers/product-controllers.js
@@ -1,6 +1,35 @@
+const { Product, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
 const productControllers = {
   getProducts: async (req, res, next) => {
-    res.render('products')
+    try {
+      const DEFAULT_LIMIT = 9
+      const page = Number(req.query.page) || 1
+      const limit = Number(req.query.limit) || DEFAULT_LIMIT
+      const offset = getOffset(limit, page)
+      const categoryId = req.query.categoryId || ''
+      const products = await Product.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: [Category],
+        where: { ...categoryId ? { categoryId } : {} },
+        limit,
+        offset
+      })
+      const categories = await Category.findAll({ raw: true })
+      const data = await products.rows.map(product => ({
+        ...product, description: product.description.substring(0, 50) + '...'
+      }))
+      return res.render('products', {
+        products: data,
+        categories,
+        categoryId,
+        pagination: getPagination(limit, page, products.count)
+      })
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
