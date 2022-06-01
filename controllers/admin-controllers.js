011efd9be115d8fe3715fe32212f54f68eb1c274
Pin Hsu Chen <paul791111@gmail.com>
2022-06-01 10:56:55 +0800

    feat: add admin users page & set admin

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,4 +1,4 @@
-const { Product, Category } = require('../models')
+const { Product, Category, User } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminControllers = {
@@ -15,10 +15,7 @@ const adminControllers = {
     try {
       const productId = req.params.id
       const product = await Product.findByPk(productId, { raw: true, nest: true, include: [Category] })
-      if (!product) {
-        req.flash('warning_msg', '商品不存在')
-        return res.redirect('back')
-      }
+      if (!product) throw new Error('商品不存在')
       return res.render('admin/product', { product })
     } catch (error) {
       next(error)
@@ -70,10 +67,7 @@ const adminControllers = {
       const productId = req.params.id
       const categories = await Category.findAll({ raw: true })
       const product = await Product.findByPk(productId, { raw: true, nest: true, include: [Category] })
-      if (!product) {
-        req.flash('warning_msg', '商品不存在')
-        return res.redirect('back')
-      }
+      if (!product) throw new Error('商品不存在')
       return res.render('admin/edit-product', { product, categories })
     } catch (error) {
       next(error)
@@ -98,10 +92,7 @@ const adminControllers = {
       }
       const filePath = await localFileHandler(file)
       const product = await Product.findByPk(productId)
-      if (!product) {
-        req.flash('warning_msg', '商品不存在')
-        return res.redirect('back')
-      }
+      if (!product) throw new Error('商品不存在')
       await product.update({
         name,
         categoryId,
@@ -114,6 +105,44 @@ const adminControllers = {
       })
       req.flash('success_messages', '成功編輯商品')
       return res.redirect('/admin/products')
+    } catch (error) {
+      next(error)
+    }
+  },
+  deleteProduct: async (req, res, next) => {
+    try {
+      const productId = req.params.id
+      const product = await Product.findByPk(productId)
+      if (!product) throw new Error('商品不存在')
+      await product.destroy()
+      req.flash('success_messages', '成功刪除商品')
+      return res.redirect('/admin/products')
+    } catch (error) {
+      next(error)
+    }
+  },
+  getUsersPage: async (req, res, next) => {
+    try {
+      const users = await User.findAll({ raw: true })
+      return res.render('admin/users', { users })
+    } catch (error) {
+      next(error)
+    }
+  },
+  setAdmin: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+      const user = await User.findByPk(userId)
+      if (!user) throw new Error('使用者不存在')
+      if (user.email === 'root@example.com') {
+        req.flash('warning_msg', '禁止變更root權限')
+        return res.redirect('/admin/users')
+      }
+      await user.update({
+        isAdmin: !user.isAdmin
+      })
+      req.flash('success_messages', '成功變更權限')
+      return res.redirect('/admin/users')
     } catch (error) {
       next(error)
     }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,10 +5,14 @@ const adminControllers = require('../../controllers/admin-controllers')
 
 router.get('/products/create', adminControllers.getCreateProductPage)
 router.get('/products/:id', adminControllers.getProduct)
+router.delete('/products/:id', adminControllers.deleteProduct)
 router.get('/products/:id/edit', adminControllers.getEditProductPage)
 router.put('/products/:id', upload.single('image'), adminControllers.editProduct)
 router.get('/products', adminControllers.getProducts)
 router.post('/products', upload.single('image'), adminControllers.createProduct)
+
+router.patch('/users/:id', adminControllers.setAdmin)
+router.get('/users', adminControllers.getUsersPage)
 
 router.get('/', (req, res) => res.redirect('/admin/products'))
 
